176e1902ab435a4d89fefd5e14052e1c3239379c
Radhika Gupte <gupter@oregonstate.edu>
2023-10-14 19:43:22 -0700

    created a schema and a model out of it

--- myApp.js
+++ myApp.js
@@ -7,6 +7,21 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+  },
+  favoriteFoods: {
+    type: [String],
+  },
+});
+Person = mongoose.model('Person', personSchema);
+
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
